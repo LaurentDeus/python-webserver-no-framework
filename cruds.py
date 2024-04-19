69f97545733edb2ffdaf250802934dadea0b0725
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-19 13:45:18 +0300

    method to get restaurant names as list

--- cruds.py
+++ cruds.py
@@ -5,3 +5,7 @@ def get_all_restaurants()->Restaurant:
     with Session(engine) as dbsession:
         restaurants = dbsession.query(Restaurant).all()        
     return restaurants
+
+def get_restaurant_names()->list:
+    restaurants = get_all_restaurants()
+    return [r.name for r in restaurants]
