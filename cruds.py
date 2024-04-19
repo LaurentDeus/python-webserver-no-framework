700ecd2eab098ddd1beb5e2b6a8ddb7a7c462608
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-19 14:02:09 +0300

    I dont Know

--- cruds.py
+++ cruds.py
@@ -1,11 +1,17 @@
 from sqlalchemy.orm import Session
 from database import *
 
-def get_all_restaurants()->Restaurant:
+
+def get_all_restaurants() -> Restaurant:
     with Session(engine) as dbsession:
-        restaurants = dbsession.query(Restaurant).all()        
+        restaurants = dbsession.query(Restaurant).all()
     return restaurants
 
-def get_restaurant_names()->list:
+
+def get_restaurant_names() -> list:
     restaurants = get_all_restaurants()
     return [r.name for r in restaurants]
+
+
+restaurants_names = get_restaurant_names()
+
