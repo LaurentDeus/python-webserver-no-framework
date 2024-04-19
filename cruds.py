b868b5ad803daaee96605ffa1fed54d91dd1c00a
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-19 17:10:52 +0300

    created method to fetch single restaurant

--- cruds.py
+++ cruds.py
@@ -1,6 +1,14 @@
+from typing import Union
 from sqlalchemy.orm import Session
 from database import *
 
+
+def get_restaurant(id:int)->Union[Restaurant,None]:
+    try:
+        with Session(engine) as dbsession:
+           return dbsession.query(Restaurant).get(id)
+    except Exception as e:
+        print(f'Error occured while fetching Restaurant with ID {id}',e)
 
 def get_all_restaurants() -> Restaurant:
     with Session(engine) as dbsession:
