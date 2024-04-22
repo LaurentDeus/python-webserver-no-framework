55083d23d4a05425c7c913ba97dd38182a95ad4c
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-22 13:54:41 +0300

    created controller for listing all menu Items for this restaurant

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-from cruds import get_all_restaurants
+from flask import Flask, render_template
+from cruds import get_all_restaurants, get_restaurant, get_restaurant_menuitems
 
 app = Flask(__name__)
 
@@ -8,9 +8,15 @@ app = Flask(__name__)
 @app.route('/restaurants')
 def restaurants():
     all_restaurants = get_all_restaurants()
-    return render_template('restaurants.htm',restaurants = all_restaurants)
+    return render_template('restaurants.htm', restaurants=all_restaurants)
 
 
+@app.route('/restaurant/<int:restaurant_id>')
+def menuitems(restaurant_id):
+    restaurant = get_restaurant(id=restaurant_id)
+    mi = get_restaurant_menuitems(restaurant_id=restaurant_id)
+    data = {'restaurant': restaurant, 'menu_items': mi}
+    return render_template('menu_items.html', **data)
 
 
 if __name__ == '__main__':
