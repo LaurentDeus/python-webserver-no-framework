18cc0674ae7922c723376da573fa17a4488e988c
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-22 12:38:58 +0300

    created all restaurants list controller

--- project.py
+++ project.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
+from cruds import get_restaurant_names
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/restaurants')
 def restaurants():
-    return "List Of Restaurants"
+    all_restaurants = get_restaurant_names()
+    return render_template('restaurants.htm',restaurants = all_restaurants)
 
 
 
