5d56b5afcc441ea9c424b075e1cbc9250f583246
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-22 15:35:31 +0300

    created Controller for editing,deleting and adding menuitem

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from cruds import get_all_restaurants, get_restaurant, get_restaurant_menuitems
+from flask import Flask, redirect, render_template, request, url_for
+from cruds import create_menuitem, get_all_restaurants, get_restaurant, get_restaurant_menuitems
 
 app = Flask(__name__)
 
@@ -17,6 +17,30 @@ def menuitems(restaurant_id):
     mi = get_restaurant_menuitems(restaurant_id=restaurant_id)
     data = {'restaurant': restaurant, 'menu_items': mi}
     return render_template('menu_items.html', **data)
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menuitem_id>/edit')
+def edit_menuitem(restaurant_id: int, menuitem_id: int):
+    return render_template('edit_menuitem.html', rid=restaurant_id, mid=menuitem_id)
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menuitem_id>/delete')
+def delete_menuitem(restaurant_id, menuitem_id):
+    return render_template('delete_menuitem.html', rid=restaurant_id, mid=menuitem_id)
+
+
+@app.route('/restaurants/<int:restaurant_id>/new_menuitem', methods=['post', 'get'])
+def add_new_menuitem(restaurant_id):
+    if request.method == 'GET':
+        return render_template('add_new_menuitem.html', rid=restaurant_id)
+    menuitem_name = request.form.get('menuitem_name')
+    description = request.form.get('description')
+    price = request.form.get('price')
+    course = request.form.get('course')
+
+    if create_menuitem(restaurant_id, menuitem_name, description, price, course):
+        return redirect(url_for('menuitems', restaurant_id=restaurant_id))
+    return render_template('add_new_menuitem.html', rid=restaurant_id)
 
 
 if __name__ == '__main__':
